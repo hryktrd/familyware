1c3d01a7589e795d40d05d69e511662410164c92
Hiroyuki Terada <hissan1983@gmail.com>
2017-03-10 15:07:34 +0900

    コンタクト追加の大枠追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {AboutPage} from '../pages/about/about';
 import {ContactPage} from '../pages/contact/contact';
 import {HomePage} from '../pages/home/home';
 import {TabsPage} from '../pages/tabs/tabs';
+import {Contacts} from "../providers/contacts";
 
 @NgModule({
     declarations: [
@@ -25,7 +26,7 @@ import {TabsPage} from '../pages/tabs/tabs';
         HomePage,
         TabsPage
     ],
-    providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+    providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, Contacts]
 })
 export class AppModule {
 }
--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import {NavController} from 'ionic-angular';
+import {Contacts} from "../../providers/contacts";
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+    selector: 'page-contact',
+    templateUrl: 'contact.html'
 })
-export class ContactPage {
+export class ContactPage implements OnInit {
 
-  constructor(public navCtrl: NavController) {
+    private myContacts: any[];
 
-  }
+    constructor(public navCtrl: NavController, public contacts: Contacts) {
+
+    }
+
+    private contactId: string;
+
+    addContact() {
+        console.log(this.contactId);
+    }
+
+    ngOnInit() {
+        this.myContacts = this.contacts.getContacts();
+        console.log(this.myContacts);
+    }
 
 }
